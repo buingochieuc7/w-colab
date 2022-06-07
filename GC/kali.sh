ecabe58f347c04e9b0c40f1d7db57b0d9739de2e
buingochieuc7 <99152917+buingochieuc7@users.noreply.github.com>
2022-06-07 15:12:33 +0700

    Update kali.sh

--- GC/kali.sh
+++ GC/kali.sh
@@ -19,7 +19,7 @@ read -p "choose ngrok region: " CRP
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull danielguerra/ubuntu-xrdp
+docker pull kalilinux/kali-rolling
 clear
 echo "===================================="
 echo "Start RDP"
@@ -36,4 +36,4 @@ echo "Keep support akuh.net thank you"
 echo "Wait 1 minute to finish bot"
 echo "===================================="
 echo "===================================="
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+docker run --rm -p 3388:3389 kalilinux/kali-rolling > /dev/null 2>&1
